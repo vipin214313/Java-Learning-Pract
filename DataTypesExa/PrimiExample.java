5dffd54ba5fe0006602f5d12af444d968df27c7f
Vipin214313 <vipin214313pri@gmail.com>
2026-07-21 08:19:31 +0530

    java codes

---
+++ DataTypesExa/PrimiExample.java
@@ -0,0 +1,13 @@
+package DataTypesExa;
+public class PrimiExample {
+    public static void main(String[] args) {
+       int a = 85;
+        int b = 75;
+        int c = 95;
+
+        float results = (float) (a + b + c) / 300;
+        System.out.println(results);
+        
+    }
+    
+}
