58756bd70ce942c46bccb5bf660b23a84a7e82c3
Vipin214313 <vipin214313pri@gmail.com>
2026-07-22 07:43:04 +0530

    java codes

--- DataTypesExa/PrimiExample.java
+++ DataTypesExa/PrimiExample.java
@@ -7,6 +7,35 @@ public class PrimiExample {
 
         float results = (float) (a + b + c) / 300;
         System.out.println(results);
+
+        // Integer Types
+        byte b1 = 10;
+        short s = 2000;
+        int i = 50000;
+        long l = 100000L;
+
+        // Decimal Types
+        float f = 5.75f;
+        double d = 19.99;
+
+        // Character Type
+        char c1= 'A';
+
+        // Boolean Type
+        boolean isJavaFun = true;
+
+        // Printing Values
+        System.out.println("Byte value: " + b1);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + c1);
+        System.out.println("Boolean value: " + isJavaFun);
+
+    System.out.println("Hellow");
+
         
     }
     
